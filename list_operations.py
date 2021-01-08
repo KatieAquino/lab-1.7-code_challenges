fc84311fed31165fa215a180e8cab702243b636b
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-01-08 00:53:10 +0000

    Complete Slice Tests

--- list_operations.py
+++ list_operations.py
@@ -1,6 +1,5 @@
 """Utilities for manipulating lists."""
 
-
 def head(input_list):
     """Return the first item of the input list.
 
@@ -10,7 +9,7 @@ def head(input_list):
       'Jan'
     """
 
-    return None
+    return input_list[0]
 
 
 def tail(input_list):
@@ -23,7 +22,7 @@ def tail(input_list):
 
     """
 
-    return []
+    return input_list[1:]
 
 
 def last(input_list):
@@ -35,8 +34,7 @@ def last(input_list):
     'Mar'
 
     """
-
-    return []
+    return input_list[-1]
 
 
 def top(input_list):
@@ -49,7 +47,7 @@ def top(input_list):
 
     """
 
-    return []
+    return input_list[0:-1]
 
 
 def first_three(input_list):
@@ -62,7 +60,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -75,7 +73,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -88,7 +86,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -101,7 +99,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -117,7 +115,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -131,7 +129,7 @@ def replace_head(input_list):
     True
 
     """
-
+    input_list[0] = 42
     pass
 
 
@@ -147,6 +145,8 @@ def replace_third_and_last(input_list):
 
     """
 
+    input_list[2] = 37
+    input_list[-1] = 37
     pass
 
 
@@ -165,7 +165,7 @@ def replace_middle(input_list):
     True
 
     """
-
+    input_list[2:-2] = [42, 37]
     pass
 
 
@@ -180,7 +180,8 @@ def delete_third_and_seventh(input_list):
     True
 
     """
-
+    del input_list[2]
+    del input_list[5]
     pass
 
 
@@ -197,7 +198,7 @@ def delete_middle(input_list):
     True
 
     """
-
+    del input_list[2:-2]
     pass
 
 
